48063dd0080795f8616ee04ae18f2b8a5646982b
Geraldo Istalin <geraldoboueres@gmail.com>
2021-10-06 09:43:34 -0300

    polished

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button, {ButtonProps} from '../components/Button/Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+ 
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  variant: 'primary',
+  label: 'Button',
+};
+
+
+export const Danger = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Danger.args = {
+  variant: 'danger',
+  label: 'Button',
+};
+
+
+export const Text = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Text.args = {
+  variant: 'text',
+  label: 'Button',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  variant: 'primary',
+  disabled: true,
+  label: 'Button',
+};
