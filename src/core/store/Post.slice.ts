4c2ec2176c217f6925a6f26e2486382db7d39e55
Geraldo Boueres <geraldoboueres@gmail.com>
2021-12-28 23:30:24 -0400

    Redux - Store

--- src/core/store/Post.slice.ts
+++ src/core/store/Post.slice.ts
@@ -5,3 +5,25 @@ interface PostSliceState {
   paginated?: Post.Paginated;
 }
 
+const initialState: PostSliceState = {
+  paginated: {
+    page: 0,
+    size: 0,
+    totalElements: 0,
+    totalPages: 1,
+    content: [],
+  },
+};
+
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    addPost(state, action: PayloadAction<Post.Summary>) {
+      state.paginated?.content?.push(action.payload);
+    },
+  },
+});
+
+export const postReducer = postSlice.reducer;
+export const { addPost } = postSlice.actions;
