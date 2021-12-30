f410e6dea55d1cd0ae89866816c58f1ccf6a4a9f
Geraldo Boueres <geraldoboueres@gmail.com>
2021-12-30 08:47:41 -0400

    Using Redux

--- src/core/store/Post.slice.ts
+++ src/core/store/Post.slice.ts
@@ -1,51 +1,56 @@
 import {
+  createAction,
   createAsyncThunk,
-  createSlice,
+  createReducer,
   isFulfilled,
   isPending,
   isRejected,
 } from "@reduxjs/toolkit";
-import { User } from "../../sdk/@types";
-import UserService from "../../sdk/services/User.service";
+import { Post, PostService } from "geraldoboueres-sdk";
 
-export const fetchEditors = createAsyncThunk(
-  "user/fetchEditors",
-  async function () {
-    const editors = await UserService.getAllEditors();
-    return editors;
+interface PostSliceState {
+  paginated?: Post.Paginated;
+  fetching: boolean;
+  counter: number;
+}
+
+const initialState: PostSliceState = {
+  fetching: false,
+  counter: 0,
+  paginated: {
+    page: 0,
+    size: 0,
+    totalElements: 0,
+    totalPages: 1,
+    content: [],
+  },
+};
+
+export const fetchPosts = createAsyncThunk(
+  "post/fetchPosts",
+  async function (query: Post.Query) {
+    const posts = await PostService.getAllPosts(query);
+    return posts;
   }
 );
 
-interface UserSliceState {
-  editors: User.EditorSummary[];
-  fetching: boolean;
-}
+export const increment = createAction("post/increment");
 
-const initialState: UserSliceState = {
-  fetching: false,
-  editors: [],
-};
-
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    const pendingActions = isPending(fetchEditors);
-    const fulfilledActions = isFulfilled(fetchEditors);
-    const rejectedActions = isRejected(fetchEditors);
-
-    builder
-      .addMatcher(pendingActions, (state) => {
-        state.fetching = true;
-      })
-      .addMatcher(fulfilledActions, (state) => {
-        state.fetching = false;
-      })
-      .addMatcher(rejectedActions, (state) => {
-        state.fetching = false;
-      });
-  },
+export const postReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(increment, (state) => {
+      state.counter++;
+    })
+    .addCase(fetchPosts.fulfilled, (state, action) => {
+      state.paginated = action.payload;
+    })
+    .addMatcher(isPending, (state) => {
+      state.fetching = true;
+    })
+    .addMatcher(isFulfilled, (state) => {
+      state.fetching = false;
+    })
+    .addMatcher(isRejected, (state) => {
+      state.fetching = false;
+    });
 });
-
-export const userReducer = userSlice.reducer;
